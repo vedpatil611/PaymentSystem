c2c831e30433ef3bf3a680879758345f79604f38
vedpatil611 <vedpatil611@gmail.com>
2022-08-23 10:38:22 +0530

    First project

---
+++ src/test/java/com/barclays/paymentsystem/PaymentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barclays.paymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
