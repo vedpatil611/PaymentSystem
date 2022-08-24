f326dc082a387371da086f4587cd791aac43432f
Ritu Pawar <70062519+ritu100@users.noreply.github.com>
2022-08-24 18:33:45 +0530

    Update MasterBillerController.java

--- src/main/java/com/barclays/paymentsystem/api/MasterBillerController.java
+++ src/main/java/com/barclays/paymentsystem/api/MasterBillerController.java
@@ -15,11 +15,21 @@ import com.barclays.paymentsystem.dto.MasterBillerDTO;
 import com.barclays.paymentsystem.exception.PaymentSystemException;
 import com.barclays.paymentsystem.service.MasterBillerService;
 
+/*
+ * MasterBillerController - Rest api for Master Biller details
+ */
 @RestController
 public class MasterBillerController {
 	
 	@Autowired
 	MasterBillerService masterBillerService;
+	
+	/**
+	 * getAllMasterBiller - Get Master Biller list
+	 * @param Master Biller - Master Biller list
+	 * @return list 
+	 * @throws PaymentSystemException
+	 */
 	
 	@GetMapping("/biller/list")
 	public ResponseEntity<List<MasterBillerDTO>> getAllMasterBiller() throws PaymentSystemException {
@@ -27,13 +37,26 @@ public class MasterBillerController {
 		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
+	/**
+	 * getMasterBillerById - Get Master Biller By Id
+	 * @param masterBillerId - Master Biller Id
+	 * @return Master Biller
+	 * @throws PaymentSystemException
+	 */
+	
 	@GetMapping("/biller/{masterBillerId}")
 	public ResponseEntity<MasterBillerDTO> getMasterBillerById(@PathVariable String masterBillerId) throws PaymentSystemException {
 		MasterBillerDTO masterBiller = masterBillerService.getMasterBiller(masterBillerId);
 		return new ResponseEntity<>(masterBiller, HttpStatus.OK);
 	}
 	 
-	/* Adding new biller to master biller list */
+	/**
+	 * addNewMasterBiller - Add new  master biller to list
+	 * @param Master Biller - Master Biller list
+	 * @return New Master Biller Code 
+	 * @throws PaymentSystemException
+	 */
+	
 	@PostMapping("/biller/new")
 	public ResponseEntity<String> addNewMasterBiller(@RequestBody MasterBillerDTO masterBillerDTO) throws PaymentSystemException {
 		String newCode = masterBillerService.addNewMasterBiller(masterBillerDTO);
