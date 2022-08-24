920d880655dce07d27973cef3beac38d56cea2ef
Vinay <ludhraniv@gmail.com>
2022-08-24 09:51:51 +0530

    For Manual Payment*

--- src/main/java/com/barclays/paymentsystem/service/PaymentServiceImpl.java
+++ src/main/java/com/barclays/paymentsystem/service/PaymentServiceImpl.java
@@ -14,21 +14,4 @@ public class PaymentServiceImpl implements PaymentService {
 	@Override
 	public void manualPayment(AccountDTO accountDTO) throws PaymentSystemException {
 		// TODO Auto-generated method stub
-		try {
-			accountService.transferBalances(request);
-			
-			TransferResult result = new TransferResult();
-			result.setAccountFromId(request.getAccountFromId());
-			result.setBalanceAfterTransfer(accountService.checkBalance(request.getAccountFromId()));
-			
-			return new ResponseEntity<>(result, HttpStatus.ACCEPTED);
-		} catch (AccountNotExistException | OverDraftException e) {
-			log.error("Fail to transfer balances, please check with system administrator.");
-			throw e;
-		} catch (CheckBalanceException cbEx) {
-			log.error("Fail to check balances after transfer, please check with system administrator.");
-			throw cbEx;
-		}
-	}
-
 }
