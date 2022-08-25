9b72eaeede80cd5e64534a4b9a0e6f5ae8a25f73
vedpatil611 <vedpatil611@gmail.com>
2022-08-25 12:55:46 +0530

    Main class doc comment

--- src/main/java/com/barclays/paymentsystem/PaymentsystemApplication.java
+++ src/main/java/com/barclays/paymentsystem/PaymentsystemApplication.java
@@ -3,6 +3,13 @@ package com.barclays.paymentsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Payment system application offer api for manager and account holder.
+ * Manager can add new master billers to list and create new list
+ * Account holders can subscribe to a service and pay them bill
+ * @author Ved
+ *
+ */
 @SpringBootApplication
 public class PaymentsystemApplication {
 
